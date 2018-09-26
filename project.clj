d67c128551e3c9e163463dbee26a561400b6972c
Gaivile <gaivile.vezeviciute@healthunlocked.com>
2018-09-26 10:04:48 +0100

    Initial commit :taco:

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject parkrun-app "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                  [enlive "1.1.6"]
+                  [ring "1.6.0"]
+                  [net.cgrand/moustache "1.1.0"]
+                  [org.clojure/java.jdbc "0.4.2"]
+                  [org.clojure/data.json "0.2.6"]
+                  [clj-http "3.5.0"]])
---
+++ src/parkrun_app/core.clj
@@ -0,0 +1,56 @@
+(ns parkrun-app.core
+  (:require [clojure.string :as string]
+            [net.cgrand.enlive-html :as html]
+            [clj-http.client :as http]))
+
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+(def scrape
+  (fetch-url "https://www.parkrun.com/cancellations/"))
+
+(def saturday "2018-09-29")
+
+(def sunday "2018-09-30")
+
+(defn get-dates
+  [scrape]
+  (->> (html/select scrape [:div.wysiwyg])
+       (first)
+       (:content)
+       (partition 2)))
+
+(defn dates-of-interest?
+  [scrape]
+  (let [date (->> scrape
+                  (first)
+                  (:content)
+                  (first))]
+    (if (or (= saturday date)
+            (= sunday date))
+      true)))
+
+(def dates (filter dates-of-interest? (get-dates scrape)))
+
+(defn extract-data
+  [dates]
+  (let [parkrun (html/select (second (first dates)) [:li])]
+    ((juxt (partial map #(->> %
+                        (:content)
+                        (second)))
+           (partial map #(->> %
+                        (:content)
+                        (first)
+                        (:attrs)
+                        (:href)))
+           (partial map #(->> %
+                        (:content)
+                        (first)
+                        (:content)
+                        (first)))) parkrun)))
+
+(extract-data dates)
+
+(partition 3 (apply interleave (extract-data dates)))
+
+dates
